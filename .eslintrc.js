ace9abe0365026bae3b74eeef98255f055214e99
Theo <theo@iconara.net>
2020-05-27 17:50:33 +0200

    Add my standard ESLint configs and apply them to the config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,13 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['warn', 'always-multiline'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'object-curly-spacing': ['error', 'never'],
+    'vue/html-closing-bracket-spacing': ['error', {startTag: 'never', endTag: 'never', selfClosingTag: 'never'}],
+    'vue/mustache-interpolation-spacing': ['error', 'never'],
+    '@typescript-eslint/member-delimiter-style': ['error', {multiline: {delimiter: 'none', requireLast: false}, singleline: {delimiter: 'comma', requireLast: false}}],
   }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -7,18 +7,18 @@ export default {
   head: {
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
   /*
   ** Global CSS
   */
@@ -33,14 +33,14 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
-    '@nuxt/typescript-build'
+    '@nuxt/typescript-build',
   ],
   /*
   ** Nuxt.js modules
   */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt'
+    'bootstrap-vue/nuxt',
   ],
   /*
   ** Build configuration
@@ -49,7 +49,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
-    }
-  }
+    extend (_config, _ctx) {
+    },
+  },
 }
