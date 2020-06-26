d7b75113cbe83289d545644645f93171f269497f
Theo <theo@iconara.net>
2020-06-26 15:03:25 +0200

    Replace the Nuxt favicon
    This is just a simple A/G thing, might have to do something more fancy later.

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg'},
       {rel: 'stylesheet', href: 'https://use.typekit.net/kbt8opn.css'},
     ],
   },
