080ed39f2db44be1044c433c91f9088ad3892104
Theo <theo@iconara.net>
2020-07-04 22:37:15 +0200

    Add tests for the analytics plugin, and add more features
    * When an utm_source parameter is found, forward it
    * When a referrer from another domain is found, forward it

--- plugins/analytics.ts
+++ plugins/analytics.ts
@@ -1,13 +1,32 @@
 import {Context} from '@nuxt/types'
 import {Route} from 'vue-router'
 
-function reportNavigation (route: Route) {
-  const src = `/analytics.js?t=${Date.now()}&path=${encodeURIComponent(route.path)}`
-  const script = document.createElement('script')
+function compileParameters (route: Route, doc: Document): string {
+  const pairs = [
+    `t=${Date.now()}`,
+    `path=${encodeURIComponent(route.path)}`,
+  ]
+  const referrer = doc.referrer
+  if (!referrer.includes(`//${doc.location.host}/`)) {
+    pairs.push(`referrer=${encodeURIComponent(referrer)}`)
+  }
+  const source = route.query.utm_source
+  if (source) {
+    const s = Array.isArray(source) ? source[source.length - 1] : source
+    if (s) {
+      pairs.push(`utm_source=${encodeURIComponent(s)}`)
+    }
+  }
+  return pairs.join('&')
+}
+
+function reportNavigation (to: Route, _from: Route, doc: Document = document) {
+  const src = `/analytics.js?${compileParameters(to, doc)}`
+  const script = doc.createElement('script')
   script.setAttribute('src', src)
   script.setAttribute('async', 'async')
-  script.addEventListener('load', () => document.body.removeChild(script))
-  document.body.appendChild(script)
+  script.addEventListener('load', () => doc.body.removeChild(script))
+  doc.body.appendChild(script)
 }
 
 export default (ctx: Context) => {
