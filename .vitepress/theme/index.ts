7438ff5eac35d078de07795bab77f44b126d74d3
Theo <theo@iconara.net>
2024-01-13 21:55:55 +0100

    Use Vitepress default theme styles without using the theme
    This might not be kosher, but let's try it. I removed some of the styles I don't need.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,8 +1,11 @@
-import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
+
+import 'vitepress/dist/client/theme-default/styles/vars.css'
+import 'vitepress/dist/client/theme-default/styles/base.css'
+import 'vitepress/dist/client/theme-default/styles/components/custom-block.css'
+import 'vitepress/dist/client/theme-default/styles/components/vp-code.css'
 import './style.css'
 
 export default {
-  extends: DefaultTheme,
   Layout,
 }
