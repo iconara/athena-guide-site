f1432e97e69e7f2cc30650424ca680a1675e4fd2
Theo <theo@iconara.net>
2024-01-14 21:12:14 +0100

    Add basic page tracking
    This works the same as the old site, more or less

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -3,6 +3,7 @@ import {useData} from 'vitepress'
 import NotFound from './NotFound.vue'
 import Article from './Article.vue'
 import Frontpage from './Frontpage.vue'
+import Analytics from './Analytics.vue'
 
 const {page, frontmatter} = useData()
 </script>
@@ -11,4 +12,7 @@ const {page, frontmatter} = useData()
   <NotFound v-if="page.isNotFound" />
   <Frontpage v-else-if="frontmatter.layout === 'home'" />
   <Article v-else />
+  <ClientContent>
+    <Analytics/>
+  </ClientContent>
 </template>
